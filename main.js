49af644390b90372945cd0644401cd2fa4eb9427
Derrick A Wood <dwood_1994@yahoo.com>
2020-02-16 21:37:49 -0600

    start/stop game

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
+let inflateButton = document.getElementById("inflate-button")
+let startButton = document.getElementById("start-button")
 let clickCount = 0
 let height = 120
 let width = 100
 let inflationRate = 20
 let maxSize = 300
+let popCount = 0
 
+
+function startGame(){
+
+  startButton.setAttribute("disabled", "true")
+  inflateButton.removeAttribute("disabled")
+
+  setTimeout(() => {
+    console.log("TIME!")
+    startButton.removeAttribute("disabled")
+    inflateButton.setAttribute("disabled", "true")
+
+    clickCount = 0
+    height = 120
+    width = 100
+
+    draw()
+
+  }, 5000)
+}
 
 
 function inflate(){
   clickCount++
-  let balloonElement = document.getElementById("balloon")
   height+= inflationRate
   width+= inflationRate
   
   if(height >= maxSize){
+    popCount++
     console.log("popped")
     width = 0
     height = 0
   }
+  draw()
+}
+
+function draw(){
+  let clickElem = document.getElementById("click-count")
+  let balloonElement = document.getElementById("balloon")
+  let popCountElem = document.getElementById("pop-count")
 
   balloonElement.style.height = height.toString()
   balloonElement.style.width = width.toString()
-
-  let clickElem = document.getElementById("click-count")
   clickElem.innerText = clickCount.toString()
+  popCountElem.innerHTML = popCount.toString()
+  
 }
